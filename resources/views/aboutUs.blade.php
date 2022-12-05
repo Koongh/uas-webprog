d96ee9e430f31347bf0815af999f260b10b9bcae
LeonardoJFN <100654621+LeonardoJFN@users.noreply.github.com>
2022-12-05 22:43:34 +0700

    Create aboutUs.blade.php

---
+++ resources/views/aboutUs.blade.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>About Us</title>
+</head>
+<body>
+    <h1>About Us</h1>
+    <h2>KSPEC MOTOR</h2>
+    Toko aksesoris motor <br>
+    <p> 
+    Kami reseller yang menjual produk sesuai dengan judul/deskripsi dari Produsen/Pabrik.
+    Motto lapak kami CEPAT KIRIM DAN SEMOGA CEPAT SAMPAI JUGA.
+    </p>
+    Alamat: Jl. Megu - Cisoka No.5, Sukatani, Kec. Cisoka, Kabupaten Tangerang, Banten 15730 <br>
+    Nomor Telp: 0812-1315-0555 <br>
+    Hours:  <br>
+    <ul>
+        <li>Senin - Jumat: 9:00am - 5:00pm</li>
+        <li>Sabtu dan Minggu: 9:00am - 3:00pm</li>
+    </ul> <br>
+    <h2>Kelompok 3</h2>
+    Topik : Aplikasi transaksi dan pendataan barang untuk toko aksesoris motor KSPEC MOTOR <br>
+    Anggota Kelompok : <br>
+    <ul>
+        <li>1. Koong Hiap (00000055136) </li>
+        <li>2. Christophorus Augusta Wangsa (00000055420) </li>
+        <li>3. Wahyu Dwi Setio Wibowo (00000055320) </li>
+        <li>4. Leonardo Jonathan Fernandez Namlay (00000058084) </li>
+        <li>5. Kenny Haryanto (00000054248) </li>
+        <li>6. Jennifer Arlene Kurniawan (00000056232) </li>
+        <li>7. Orde Gilbert Wiguna (00000055282) </li>
+    </ul>
+</body>
+</html>
