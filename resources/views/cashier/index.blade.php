738c584717eb92c51acedd3a0bcae4e54d6dc0c3
LeonardoJFN <100654621+LeonardoJFN@users.noreply.github.com>
2022-12-10 20:16:55 +0700

    Add : styling for nota and cashier

--- resources/views/cashier/index.blade.php
+++ resources/views/cashier/index.blade.php
@@ -7,13 +7,13 @@
 
 <!-- contoh untuk resource -->
 <!-- <link rel="stylesheet" href="../../css/home.css" /> -->
-
+<!-- <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.10.25/css/jquery.dataTables.css%22%3E"> -->
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
 <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
 @endsection
 
 @section('content')
-    <div class="products grid grid-cols-2">
+    <div class="products grid grid-cols-2 px-3 py-3 md:flex w-full space-x-4 space-y-4">
         <table class="table w-full"  id="products_table">
             <thead>
                 <tr>
@@ -29,14 +29,14 @@
                         <td>{{$product->name}}</td>
                         <td>{{$product->stock}}</td>
                         <td>{{$product->price}}</td>
-                        <td><button onclick="addToCart({{$product->id}})" {{ $product->stock == 0 ? "disabled" : ""}}>Add to cart</button></td>
+                        <td><button class="bg-blue-500 disabled:bg-gray-500 hover:bg-blue-700 text-white font-bold py-1 px-2 rounded" onclick="addToCart({{$product->id}})" {{ $product->stock == 0 ? "disabled" : ""}}>Add to cart</button></td>
                     </tr>
                 @endforeach
             </tbody>
         </table>
     </div>
-    <div class="pos" border=1>
-        <table>
+    <div class=" grid grid-cols-2 px-3 py-3 md:flex w-full space-x-4 space-y-4" border=1>
+        <table class="table w-full">
             <thead>
                 <tr>
                     <th>Del</th>
@@ -47,6 +47,8 @@
                 </tr>
             </thead>
             <tbody id="poscart">
+                <td class="text-center" colspan="5 ">
+                    <h1 class="text-center text-2xl font-bold text-gray-500" id="emptycart">Shopping Cart</h1></td>
             </tbody>
             <tfoot>
                 <tr>
@@ -55,18 +57,45 @@
                 </tr>
             </tfoot>
         </table>
+        
+        
+    </div>
+    <!-- form dan submit buttonnya -->
+    <div class="flex   items-end px-4">
         <form action="/cashier" method="post">
             @csrf
             <input type="text" name="items_lists" id="items_lists" hidden/>
-            <button class="bg-green-300 disabled:bg-black text-black font-bold py-2 px-4 rounded-full" id="checkout" type="submit" disabled>Konfirmasi Bayar</button>
+            <button class="bg-green-300 disabled:bg-red-300 hover:bg-green-400 text-black font-bold py-2 px-4 rounded-full" id="checkout" type="button" disabled onclick="confirmCheckout()" >Keranjang Kosong</button>
         </form>
     </div>
+
 @endsection
 
 <!-- Script javascriptnya disini gais -->
 @section('custom-js')
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.13.1/js/jquery.dataTables.js"></script>
+<script src="//cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 <script>
+    // ini buat panggil alertnya
+    function confirmCheckout() {
+        let $form = $(this).closest('form');
+        Swal.fire({
+            title: 'Apakah anda yakin?',
+            text: "Pastikan barang yang akan dibeli sudah benar!",
+            icon: 'warning',
+            showCancelButton: true,
+            confirmButtonColor: '#3085d6',
+            cancelButtonColor: '#d33',
+            confirmButtonText: 'Ya, saya yakin!'
+        }).then((result) => {
+            if (result.isConfirmed) {
+                $form.submit();
+            }
+        })
+    }
+
+
+
     $(document).ready( function () {
         $('#products_table').DataTable();
     } );
@@ -103,6 +132,7 @@
     function updateForm(){
         var form_value = "";
         $('#checkout').prop('disabled', true);
+        $('#checkout').text("Keranjang Kosong");
         items.forEach((item) => {
             if(item.stock > 0) form_value += (item.id + "#" + item.stock + "@" + (item.stock*item.price) + "/");
         });
@@ -110,7 +140,13 @@
         $("#items_lists").val(form_value);
 
         //disable submit button if form_value is empty
-        if(form_value != "") $('#checkout').prop('disabled', false);
+        // change checkout button text to "Cart is empty" if form_value is empty
+        
+        if(form_value != "") {
+            $('#checkout').prop('disabled', false);
+            $('#emptycart').text("");
+            $('#checkout').text("Konfirmasi Bayar");
+        }
     }
 
     function updateCart(){
@@ -119,7 +155,7 @@
         var sum = 0;
         items.forEach((item) => {
             if(item.stock > 0){
-                var temp = '<tr><td><button class="bg-gray-400 hover:bg-red-500 text-white font-bold rounded-full justify-center items-center" style="width: 100%" onclick="removeFromCart(' + item.id + ')">-</button></td><td><p>' + item.stock + '</p></td><td><p>' + item.name + '</p></td><td><p>' + item.price + '</p></td><td><p>' + (item.stock * item.price) + '</p></td></tr>';
+                var temp = '<tr><td><button class="bg-gray-400 hover:bg-red-500 text-white font-bold rounded-full justify-center items-center" style="width: 100%" onclick="removeFromCart(' + item.id + ')">-</button></td><td><p>' + item.stock + '</p></td><td><p>' + item.name + '</p></td><td><p>' + item.price + '</p></td><td><p>' + new Intl.NumberFormat('id-ID', { style: 'currency', currency: 'IDR'}).format(item.stock * item.price) + '</p></td></tr>';
                 $('#poscart').append(temp);
                 sum += (item.stock * item.price);
             }
--- resources/views/cashier/nota.blade.php
+++ resources/views/cashier/nota.blade.php
@@ -7,38 +7,44 @@
 
 <!-- contoh untuk resource -->
 <!-- <link rel="stylesheet" href="../../css/home.css" /> -->
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.13.1/css/dataTables.bootstrap5.min.css">
+<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/5.2.0/css/bootstrap.min.css">
 @endsection
 
 @section('content')
-<h1>ORDER ID : {{$id}}</h1>
+<div class="flex flex-col">
+    <h1 class="text-center text-2xl font-bold text-gray-500">ORDER ID : {{$id}}</h1>
+    <div class=" grid grid-cols-2 px-3 py-3 md:flex w-full space-x-4 space-y-4">
+        <table class="table w-full table-striped table-bordered">
+            <thead>
+                <tr>
+                    <th>Qty</th>
+                    <th>Nama Item</th>
+                    <th>Harga</th>
+                    <th>Jumlah</th>
+                </tr>
 
-<table>
-    <thead>
-        <tr>
-            <th>Qty</th>
-            <th>Nama Item</th>
-            <th>Harga</th>
-            <th>Jumlah</th>
-        </tr>
-    </thead>
-    <tbody>
-        @foreach($items_lists as $item)
-            <tr>
-                <td>{{$item[1]}}</td>
-                <td>{{$item[3]}}</td>
-                <td>{{$item[4]}}</td>
-                <td>{{$item[2]}}</td>
-            </tr>
-        @endforeach
-    </tbody>
-    <tfoot>
-        <tr>
-            <td colspan="3">Total</td>
-            <td>{{$sum}}</td>
-        </tr>
-    </tfoot>
-</table>
-    
+        
+            </thead>
+            <tbody>
+                @foreach($items_lists as $item)
+                    <tr>
+                        <td>{{$item[1]}}</td>
+                        <td>{{$item[3]}}</td>
+                        <td>{{$item[4]}}</td>
+                        <td>Rp. {{number_format($item[2],2,',','.')}}</td>
+                    </tr>
+                    @endforeach
+            </tbody>
+            <tfoot>
+                <tr>
+                    <td colspan="3">Total</td>
+                    <td>{{$sum}}</td>
+                </tr>
+            </tfoot>
+        </table>
+    </div>  
+</div>
 @endsection
 
 <!-- Script javascriptnya disini gais -->
