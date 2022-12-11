6d68cb1d58cd2e0ff7bc73d3a82ccdfd38072ba5
0rdegbt <ordegilbet@gmail.com>
2022-12-11 23:47:37 +0700

    fix: responsive about us

--- resources/views/aboutUs.blade.php
+++ resources/views/aboutUs.blade.php
@@ -31,12 +31,12 @@
         text-center
         text-3xl
         font-extrabold text-transparent bg-clip-text bg-gradient-to-l  from-[#3FC1C9] to-[#1f2937]"><q>CEPAT KIRIM DAN SEMOGA CEPAT SAMPAI JUGA.</q></h3>
-    <section class="h-2/3  text-gray-600 body-font relative mt-4 mb-8 shadow-md">
-        <div class="absolute w-2/3 inset-0 bg-gray-300">
+    <section class="h-fit lg:h-2/3  sm:h-fit md:h-3/4 md:block md:mb-10 text-gray-600 body-font relative mt-4 mb-8 shadow-md">
+        <div class="h-full lg:absolute lg:w-2/3 lg:h-full md:w-full sm:h-full md:h-2/3 inset-0 bg-gray-300">
             <iframe width="100%" height="100%" frameborder="0" marginheight="0" marginwidth="0" title="map" scrolling="no" src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d15864.011709614377!2d106.4225311!3d-6.2633432!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0xea281a0543b3452a!2sKSPEC%20MOTOR!5e0!3m2!1sid!2sid!4v1670440594100!5m2!1sid!2sid" style="filter: contrast(1.2) opacity(0.75);"></iframe>
         </div>
-        <div class="container h-full mx-auto flex">
-            <div class="lg:w-1/3 h-full md:w-1/2 h-full bg-white p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
+        <div class="container lg:h-full md:h-fit md:relative bottom-0 mx-auto flex">
+            <div class="lg:w-1/3 lg:h-full md:h-fit  bg-white p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10 shadow-md">
                 <h1 class=" 
                     text-center 
                     text-4xl
@@ -69,7 +69,7 @@
                 <ul class="
                         list-disc
                         text-gray-900 
-                        text-md mt-4 mb-1 ml-10
+                        text-md mt-4 mb-5 ml-10
                         text-left
                         font-medium">
                     <li>Senin - Jumat    : 9:00am - 5:00pm</li>
